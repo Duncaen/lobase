2be5a2a18f2bd36fbf2675fb81329bedd5a07168
Duncaen <mail@duncano.de>
2017-03-06 20:42:56 +0100

    mk/bsd.subdir.mk: behave more like the original

--- mk/bsd.subdir.mk
+++ mk/bsd.subdir.mk
@@ -1,12 +1,23 @@
-SUBDIR_TARGETS = all clean install depend regress
+SUBDIR_MFLAGS = -I$(TOPDIR) -I$(TOPDIR)/mk
+SUBDIR_TARGETS = all clean cleandir includes depend obj tags manlint
+SKIPDIR ?=
 
-$(SUBDIR_TARGETS): $(SUBDIR)
+$(SUBDIR_TARGETS): _SUBDIRUSE
+
+_SUBDIRUSE:
+ifneq (,$(SUBDIR))
+	@for d in $(SUBDIR); do \
+		skip=; for s in $(SKIPDIR); do \
+			[ "$$s" = "$$d" ] && skip=1; \
+		done; \
+		[ "$$skip" ] && echo "($$d skipped)" && continue; \
+		echo "===> $$d"; \
+		$(MAKE) $(SUBDIR_MFLAGS) -C $$d $(MAKECMDGOALS) || exit 1; \
+	done
 
 $(SUBDIR):
-	@cd $@ && $(MAKE) -I$(TOPDIR) -I$(TOPDIR)/mk \
-		$(filter-out $@,$(MAKECMDGOALS))
+	@echo "===> $@"; \
+	$(MAKE) $(SUBDIR_MFLAGS) -C $@ all || exit 1
+endif
 
-$(BROKEN):
-	@echo "skipping $@"
-
-.PHONY: all clean install $(SUBDIR)
+.PHONY: _SUBDIRUSE $(SUBDIR_TARGETS) $(SUBDIR)
