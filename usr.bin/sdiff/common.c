dd1611d7e5bade415f1fd4e559b06b222c24786e
Duncaen <mail@duncano.de>
2017-03-08 06:28:28 +0100

    usr.bin/sdiff: import

---
+++ usr.bin/sdiff/common.c
@@ -0,0 +1,20 @@
+/*	$OpenBSD: common.c,v 1.4 2006/05/25 03:20:32 ray Exp $	*/
+
+/*
+ * Written by Raymond Lai <ray@cyth.net>.
+ * Public domain.
+ */
+
+#include <err.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "common.h"
+
+void
+cleanup(const char *filename)
+{
+	if (unlink(filename))
+		err(2, "could not delete: %s", filename);
+	exit(2);
+}
