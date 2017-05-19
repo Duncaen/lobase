6488f4f09ed83692451bae5a666ccf1369b39a1a
Duncaen <mail@duncano.de>
2017-05-19 18:01:56 +0200

    usr.bin/ul: update to OPENBSD_6_1

--- usr.bin/ul/ul.c
+++ usr.bin/ul/ul.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ul.c,v 1.21 2016/03/26 08:59:29 natano Exp $	*/
+/*	$OpenBSD: ul.c,v 1.23 2016/10/16 11:28:54 jca Exp $	*/
 /*	$NetBSD: ul.c,v 1.3 1994/12/07 00:28:24 jtc Exp $	*/
 
 /*
@@ -364,7 +364,7 @@ flushln(void)
 	}
 	if (lastmode != NORMAL)
 		msetmode(0);
-	if (must_overstrike && hadmodes)
+	if (must_overstrike && hadmodes && !iflag)
 		overstrike();
 	putwchar(L'\n');
 	if (iflag && hadmodes)
@@ -382,7 +382,6 @@ flushln(void)
 void
 overstrike(void)
 {
-	wchar_t wc;
 	int i, j, needspace;
 
 	putwchar(L'\r');
