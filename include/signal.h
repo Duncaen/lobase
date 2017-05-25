31f0970ad6c2a7f1b3ba6e0493ff2df44888f5fb
Duncaen <mail@duncano.de>
2017-05-25 21:10:14 +0200

    configure: add checks for sys_siglist and sys_signame

--- include/signal.h
+++ include/signal.h
@@ -5,7 +5,20 @@
 
 #define SIGINFO SIGUSR1
 
-extern const char *const sys_signame[_NSIG];
+#ifndef _NSIG
+# ifdef NSIG
+#  define _NSIG NSIG
+# else
+#  define _NSIG 128
+# endif
+#endif
+
+#if !HAVE_DECL_SYS_SIGLIST
 extern const char *const sys_siglist[_NSIG];
+#endif /* !HAVE_DECL_SYS_SIGLIST */
+
+#if !HAVE_DECL_SYS_SIGNAME
+extern const char *const sys_signame[_NSIG];
+#endif /* !HAVE_DECL_SYS_SIGNAME */
 
 #endif /* _COMPAT_SIGNAL_H_ */
