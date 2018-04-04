52435f6573f2747df0c41ebc2ba521a167d78af1
Duncaen <mail@duncano.de>
2018-04-04 16:56:51 +0200

    lib/libopenbsd: update to OPENBSD_6_3

--- lib/libopenbsd/include/namespace.h
+++ lib/libopenbsd/include/namespace.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: namespace.h,v 1.10 2016/05/07 19:05:22 guenther Exp $	*/
+/*	$OpenBSD: namespace.h,v 1.12 2018/01/18 08:23:44 guenther Exp $	*/
 
 #ifndef _LIBC_NAMESPACE_H_
 #define _LIBC_NAMESPACE_H_
@@ -136,6 +136,8 @@
 /* #include <sys/cdefs.h>	/1* for __dso_hidden and __{weak,strong}_alias *1/ */
 #define	__dso_hidden	__attribute__((__visibility__("hidden")))
 
+#define	__dso_protected		__attribute__((__visibility__("protected")))
+
 #define	HIDDEN(x)		_libc_##x
 /* #define	CANCEL(x)		_libc_##x##_cancel */
 /* #define	WRAP(x)			_libc_##x##_wrap */
@@ -149,16 +151,43 @@
 /* #define	PROTO_CANCEL(x)		__dso_hidden typeof(x) HIDDEN(x), \ */
 /* 					x asm(CANCEL_STRING(x)) */
 /* #define	PROTO_WRAP(x)		PROTO_NORMAL(x), WRAP(x) */
+/* #define	PROTO_PROTECTED(x)	__dso_protected typeof(x) x */
 
 /* #define	DEF_STRONG(x)		__strong_alias(x, HIDDEN(x)) */
 /* #define	DEF_WEAK(x)		__weak_alias(x, HIDDEN(x)) */
 /* #define	DEF_CANCEL(x)		__weak_alias(x, CANCEL(x)) */
 /* #define	DEF_WRAP(x)		__weak_alias(x, WRAP(x)) */
 /* #define	DEF_SYS(x)		__strong_alias(_thread_sys_##x, HIDDEN(x)) */
+/* #ifdef __clang__ */
+/* #define	DEF_BUILTIN(x)		__asm("") */
+/* #define	BUILTIN			__dso_protected */
+/* #else */
+/* #define	DEF_BUILTIN(x)		DEF_STRONG(x) */
+/* #define	BUILTIN */
+/* #endif */
 
 #define	MAKE_CLONE(dst, src)	__dso_hidden typeof(dst) (dst) \
 				__attribute__((alias (__STRING(src))))
 
+#if 0
+/*
+ * gcc and clang will generate calls to the functions below.
+ * Declare and redirect them here so we always go
+ * directly to our hidden aliases.
+ */
+#include <sys/_types.h>
+BUILTIN void	*memmove(void *, const void *, __size_t);
+BUILTIN void	*memcpy(void *__restrict, const void *__restrict, __size_t);
+BUILTIN void	*memset(void *, int, __size_t);
+BUILTIN void	__stack_smash_handler(const char [], int __unused);
+#ifndef __clang__
+PROTO_NORMAL(memmove);
+PROTO_NORMAL(memcpy);
+PROTO_NORMAL(memset);
+PROTO_NORMAL(__stack_smash_handler);
+#endif
+#undef	BUILTIN
+#endif
 
 #endif  /* _LIBC_NAMESPACE_H_ */
 
